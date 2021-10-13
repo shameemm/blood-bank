319e3fb5e9b58fff0b27c050f5bf37e9c3c71dbb
shameemm <mandayappuramshameem@gmail.com>
2021-10-13 18:33:16 +0530

    ready to deploy

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -33,9 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django_heroku',
-    'django_herokubbank',
-    'accounts.apps.AccountsConfig',
+    
     'bbank.apps.BbankConfig',
     'django.contrib.admin',
     'django.contrib.auth',
